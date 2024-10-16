b33d477d2f84f25f3261be1033f54555bff05d18
Matteo Vollero <m.vollero96@gmail.com>
2024-10-16 18:05:23 +0200

    Updated serializers and view

--- BE/cryptoPayBE/transactions/serializers.py
+++ BE/cryptoPayBE/transactions/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from rest_framework import status
+from rest_framework.response import Response
 from .models import Transaction
 
 
@@ -6,3 +8,10 @@ class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaction
         fields = "__all__"
+        
+    def post(self, request):
+        serializedTxn = TransactionSerializer(data=request.data)
+        if serializedTxn.is_valid():
+            serializedTxn.save()
+            return Response(serializedTxn.data, status=status.HTTP_201_CREATED)
+        return Response(serializedTxn.errors, status=status.HTTP_400_BAD_REQUEST)
--- BE/cryptoPayBE/transactions/views.py
+++ BE/cryptoPayBE/transactions/views.py
@@ -1,5 +1,3 @@
-from rest_framework import status
-from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import Transaction
 from .serializers import TransactionSerializer
@@ -8,8 +6,4 @@ from .serializers import TransactionSerializer
 # Create your views here.
 @api_view(["POST"])
 def create_transaction(request):
-    serializedTxn = TransactionSerializer(data=request.data)
-    if serializedTxn.is_valid():
-        serializedTxn.save()
-        return Response(serializedTxn.data, status=status.HTTP_201_CREATED)
-    return Response(serializedTxn.errors, status=status.HTTP_400_BAD_REQUEST)
+    return TransactionSerializer.post(request)
